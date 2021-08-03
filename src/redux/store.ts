72a1d8e70d018fef91a2900755ec1ab066aff9c3
ryokryok <mrozisan@gmail.com>
2021-08-04 02:08:57 +0900

    add reference setting redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { userExposureReducer } from './userExposureSlice'
+import { referenceExposureReducer } from './referenceExposureSlice'
 
 export const store = configureStore({
   reducer: {
     userExposure: userExposureReducer,
+    referenceExposure: referenceExposureReducer,
   },
 })
 
